d913de5dd7933df976edaff40e91f3f540202b6b
maeponyo <maeponyo@gmail.com>
2014-06-19 13:47:24 +0900

    トピックス検索の準備

--- js/newssample.js
+++ js/newssample.js
@@ -13,7 +13,7 @@
   {
       if( searcher.results )
       {
-          var content = document.getElementById( 'content' );
+          var content = document.getElementById( 'content2' );
           for( var i = 0; i < searcher.results.length; i++ )
           {
               content.appendChild( searcher.results[ i ].html );
@@ -25,3 +25,18 @@
   google.setOnLoadCallback( onLoad );
 
 
+
+
+
+ var stap = document.getElementById('stap');
+ var ES = document.getElementById('ES');
+ var iPS = document.getElementById('iPS');
+ var syuudann = document.getElementById('syuudann');
+ var cazino = document.getElementById('cazino');
+ var word2 = document.getElementById('word');
+ var word = word2.value;
+ var writeresult = document.getElementById('write');
+
+ function doo(){
+  //検索した時にすでにあるものだとその部分を表示できるようにしたい
+ }
