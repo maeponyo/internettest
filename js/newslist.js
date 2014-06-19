7d9b2259c5a906cd80600e10fe11c631ae11f33f
maeponyo <maeponyo@gmail.com>
2014-06-19 13:53:52 +0900

    Create newslist.js

---
+++ js/newslist.js
@@ -0,0 +1,12 @@
+var stap = document.getElementById('stap');
+ var ES = document.getElementById('ES');
+ var iPS = document.getElementById('iPS');
+ var syuudann = document.getElementById('syuudann');
+ var cazino = document.getElementById('cazino');
+ var word2 = document.getElementById('word');
+ var word = word2.value;
+ var writeresult = document.getElementById('write');
+
+ function doo(){
+  //検索した時にすでにあるものだとその部分を表示できるようにしたい
+ }
