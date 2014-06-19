998a00c22dcb8375f4f9d152fc3fa8d07baad3c2
maeponyo <maeponyo@gmail.com>
2014-06-19 14:08:46 +0900

    とりあえず休憩

--- js/newslist.js
+++ js/newslist.js
@@ -3,10 +3,11 @@ var stap = document.getElementById('stap');
  var iPS = document.getElementById('iPS');
  var syuudann = document.getElementById('syuudann');
  var cazino = document.getElementById('cazino');
- var word2 = document.getElementById('word');
+ var word2 = document.getElementById('word'); //入力した検索語
  var word = word2.value;
  var writeresult = document.getElementById('write');
 
  function doo(){
   //検索した時にすでにあるものだとその部分を表示できるようにしたい
+  if(word=)
  }
