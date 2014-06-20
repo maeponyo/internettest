d0890b3e3faa6278c9da2c7d0c9020be5457f149
maeponyo <maeponyo@gmail.com>
2014-06-21 06:36:19 +0900

    Create newssearch.js

---
+++ js/newssearch.js
@@ -0,0 +1,27 @@
+ var content = document.getElementById( 'content3' );
+
+  function onLoad()
+  {
+      var restriction = { topic:'h', ned:'jp' };
+
+      var newsSearch = new google.search.NewsSearch();
+      newsSearch.setSearchCompleteCallback( this, SearchComplete, [ newsSearch ] );
+      newsSearch.setRestriction( google.search.Search.RESTRICT_EXTENDED_ARGS, restriction );
+
+      newsSearch.execute( '' );
+  }
+
+  function SearchComplete( searcher )
+  {
+      if( searcher.results )
+      {
+         
+          for( var i = 0; i < searcher.results.length; i++ )
+          {
+              content.appendChild( searcher.results[ i ].html );
+          }
+      }
+  }
+
+  google.load( 'search', '1' );
+  google.setOnLoadCallback( onLoad );
