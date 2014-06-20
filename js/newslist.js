a8a4a9d57b98cce6d7b625810c9124f6d00afde1
maeponyo <maeponyo@gmail.com>
2014-06-20 13:13:49 +0900

    API動かないが組み込み終了

--- js/newslist.js
+++ js/newslist.js
@@ -6,16 +6,22 @@ var stap = document.getElementById('stap');
  var word2 = document.getElementById('word'); //入力した検索語
  var word = word2.value;
  var writeresult = document.getElementById('write');
+ google.load("search", "1");
 
- function doo(){
+ function dooo(){
   //検索した時にすでにあるものだとその部分を表示できるようにしたい
   if(word="stap" && word="STAP" ){
   	//stapの内容を見せる
+  	alert("stap");
   }
   else if(word="ES細胞" && word="ES"){
-
+     alert("ES")
   }
   else{
-  	
-  }
+      // Call this function when the page has been loaded
+        var searchControl = new google.search.SearchControl(word);
+        searchControl.addSearcher(new google.search.WebSearch(word));
+        searchControl.addSearcher(new google.search.NewsSearch(word));
+        searchControl.draw(document.getElementById("searchcontrol"));
+  		}
  }
