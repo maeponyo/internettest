91ae950bce918526ccdebca0d2b93a7db3b5bbc7
maeponyo <maeponyo@gmail.com>
2014-06-21 13:40:20 +0900

    検索結果と幹乳児の対応できた

--- js/newslist.js
+++ js/newslist.js
@@ -3,16 +3,55 @@
  var iPS = $('#iPS').html();
  var syuudann = $('#syuudann').html();
  var cazino = $('#cazino').html();
+ var dna = $("#DNA").html();
+ var D3 = $("#3D").html();
+ var ip = $("#IP").html();
  var writeresult = $('#write').html();
- var word2 = document.getElementById('word'); //入力した検索語
- var word = word2.value;
- var content3 = document.getElementById( 'content3' );
- var wordcontent = document.getElementById("content4");
+ var wordcontent = document.getElementById("wordcontent");
+ var word;
  
-function OnLoad()
-  {
-    
-      // 検索コントロールを作成する
+  // Search APIをロードする
+  google.load( 'search', '1' );
+
+ function dooo(){
+  var word2 = document.getElementById('word'); //入力した検索語
+  var word = word2.value;
+  //wordに何も入力されていないとき
+  if(word.length == 0){
+    $("#searchcontrol").html("<p>検索したい単語を入力してから検索ボタンを押してください</p>");
+    $("#wordcontent").html("<p></p>");
+  }
+  else{
+  //検索した時にすでにあるものだとその部分を表示する
+  if(word == "stap" || word == "STAP" ){
+  	//stapの内容を見せる
+  	 $("#searchcontrol").html(stap);
+  }
+  else if(word=="es" || word=="ES"){
+      $("#searchcontrol").html(ES);
+  }
+  else if(word=="iPS" || word=="ips" || word=="iPS細胞"){
+    $("#searchcontrol").html(iPS);
+  }
+  else if(word=="集団的自衛権"){
+    $("#searchcontrol").html(syuudann);
+  }
+  else if(word=="カジノ"){
+    $("#searchcontrol").html(cazino);
+  }
+  else if(word=="DNA" || word=="dna"){
+    $("#searchcontrol").html(dna);
+  }
+  else if(word=="3Dプリンタ" || word == "3dプリンタ"){
+    $("#searchcontrol").html(D3);
+  }
+  else if(word=="ipアドレス" || word == "IPアドレス"){
+    $("#searchcontrol").html(ip);
+  }
+  else{
+    $("#searchcontrol").html("<h3>" +word + "</h3>");
+  }
+     // 検索コントロールを作成する
       var searchControl = new google.search.SearchControl();
 
       // 検索コントロールにWeb検索のサーチャーを追加する
@@ -23,25 +62,6 @@ function OnLoad()
 
       // 検索を実行する
       searchControl.execute( word );
-  }
-
-
-  // Search APIをロードする
-  google.load( 'search', '1' );
-
- function dooo(){
-  //検索した時にすでにあるものだとその部分を表示できるようにしたい
-  if(word=="stap" && word=="STAP" ){
-  	//stapの内容を見せる
-  	 $("#searchcontrol").html(stap);
-  }
-  else if(word=="ES細胞" && word=="ES"){
-      $("#searchcontrol").html(ES);
-  }
-  else{
-    $("#searchcontrol").html(iPS);
-    OnLoad();
-}
-}
+}}
 
  
